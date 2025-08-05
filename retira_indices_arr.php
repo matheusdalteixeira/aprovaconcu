2e5726ac4500e7684a45e335bbb2aaf6f134e2e3
Matheus de Ávila Lins Teixeira <matheus.dal.teixeira@gmail.com>
2025-08-05 15:59:31 -0300

    Criar retira_indices_arr.php

---
+++ retira_indices_arr.php
@@ -0,0 +1,33 @@
+<?php
+
+        function retira_indices_arr($p1, $p2){
+
+                $v_vars = [$p1, $p2];
+
+                $t_vars = ["array","string"];
+
+                if(valida_seq_tipo($v_vars, $t_vars)){
+
+                        $a = -1;
+
+                        while(isset($p1[++$a])){
+
+                                if($p1[$$a] == $p2){
+
+                                        unset($p1[$a]);
+
+                                }
+
+                        }
+
+                        return $p1;
+
+                }
+
+                else
+
+                        return false;
+
+        }
+
+?>
